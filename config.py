a84c15f622cbe873195b27a43cceee1bc985bb78
Abid Abdullah <161892101+AbidAbdullah1425@users.noreply.github.com>
2024-10-30 22:13:20 +0600

    Update config.py

--- config.py
+++ config.py
@@ -5,10 +5,10 @@ id_pattern = re.compile(r'^.\d+$')
 
 class Config(object):
     # pyro client config
-    API_ID    = os.environ.get("API_ID", "")  # ⚠️ Required
-    API_HASH  = os.environ.get("API_HASH", "") # ⚠️ Required
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "") # ⚠️ Required
-    FORCE_SUB = os.environ.get('FORCE_SUB', '') # ⚠️ Required
+    API_ID    = os.environ.get("API_ID", "26254064")  # ⚠️ Required
+    API_HASH  = os.environ.get("API_HASH", "72541d6610ae7730e6135af9423b319c") # ⚠️ Required
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7581285542:AAFASkPPL7R9mTO_uHC4iuignWs3xMSp2oo") # ⚠️ Required
+    FORCE_SUB = os.environ.get('FORCE_SUB', 'AnimeQuestX') # ⚠️ Required
     AUTH_CHANNEL = int(FORCE_SUB) if FORCE_SUB and id_pattern.search(
     FORCE_SUB) else None
    
@@ -17,10 +17,10 @@ class Config(object):
     DB_NAME  = os.environ.get("DB_NAME","SnowEncoderBot") 
 
     # Other Configs 
-    ADMIN = int(os.environ.get("ADMIN", "0")) # ⚠️ Required
-    LOG_CHANNEL = int(os.environ.get('LOG_CHANNEL', '0')) # ⚠️ Required
+    ADMIN = int(os.environ.get("ADMIN", "5296584067")) # ⚠️ Required
+    LOG_CHANNEL = int(os.environ.get('LOG_CHANNEL', '-1002197279542')) # ⚠️ Required
     BOT_UPTIME = BOT_UPTIME  = time.time()
-    START_PIC = os.environ.get("START_PIC", "https://graph.org/file/15e82d7e665eccc8bd9c5.jpg")
+    START_PIC = os.environ.get("START_PIC", "https://envs.sh/IZG.jpg")
 
     # wes response configuration     
     WEBHOOK = bool(os.environ.get("WEBHOOK", True))
